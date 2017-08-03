84ecde7048b3143960e07942b8dd7770ac17d65c
Jillian Pichocki <jpichocki@gmail.com>
2017-08-03 10:38:44 -0500

    created functions in JS

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+function btnPlus() {
+    document.calculator.answer.value += "+";
+}
+
+function btnSub() {
+    document.calculator.answer.value += "-";
+}
+function btnClear() {
+    document.calculator.answer.value = "";
+}
+function btnMult() {
+    document.calculator.answer.value += "*";
+}
+function btnDiv() {
+    document.calculator.answer.value += "/";
+}
